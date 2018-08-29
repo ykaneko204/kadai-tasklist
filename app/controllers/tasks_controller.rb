e4da7daaf9a8c4a3352fe8b73e96040dc56af6a5
yuki.kaneko <ykaneko204@gmail.com>
2018-08-29 05:01:39 +0000

    User login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,56 +1,53 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
 
-  def index
-    @tasks = Task.all.page(params[:page]).per(5)
-  end
-  
-  def show
-  end
-  
-  def new
-    @task = Task.new
-  end
-  
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = "Task が正常に登録されました"
-      redirect_to @task
+      redirect_to root_url
     else
+      @tasks = current_user.tasks.order("created_at DESC").page(params[:page])
       flash.now[:danger] = "Task が登録されませんでした"
-      render :new
+      render "toppages/index"
     end
   end
   
   def edit
+    @task = current_user.tasks.find(params[:id])
+    @user = current_user
   end
   
   def update
+    @task = current_user.tasks.find(params[:id])
+    
     if @task.update(task_params)
       flash[:success] = "Task が正常に更新されました"
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = "Task が更新されませんでした"
-      render :edit
+      render "toppages/index"
     end    
   end
   
   def destroy
     @task.destroy
-    
     flash[:success] = "Task は正常に削除されました"
-    redirect_to tasks_url
+    redirect_back(fallback_location: root_path)
   end
   
   private
   
-  def set_task
-    @task = Task.find(params[:id])
-  end
-
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
-
   root to: 'toppages#index'
   
-  resources :tasks
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  
+  get "signup", to: "users#new"
+  resources :users, only: [:show, :new, :create]
+  
+  resources :tasks, only: [:create, :edit, :update, :destroy]
 end
