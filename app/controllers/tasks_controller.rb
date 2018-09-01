20deba35baf31484360b40597f5fa4afef39e36a
yuki.kaneko <ykaneko204@gmail.com>
2018-09-01 19:57:22 +0000

    ログインユーザかどうかのチェックを追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:create, :edit, :update, :destroy]
 
   def create
     @task = current_user.tasks.build(task_params)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:show]
+  before_action :login_user, only: [:show]
   
   def show
     @user = User.find(params[:id])
@@ -24,6 +25,13 @@ class UsersController < ApplicationController
   end
   
   private
+  
+  def login_user
+    @user = User.find(params[:id])
+    if current_user != @user
+      redirect_to root_url
+    end
+  end
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
